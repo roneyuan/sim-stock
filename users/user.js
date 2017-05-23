1dbecf3abe4a60945aadeaa3463328e9c814c21f
roneyuan <roneyuan_m03@hotmail.com>
2017-05-22 23:02:18 -0400

    Try fixing unable to install bcrypt on heroku

--- users/user.js
+++ users/user.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const findOrCreate = require('mongoose-find-or-create');
 Schema = mongoose.Schema;
